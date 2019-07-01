a4ab567a7f9cb8d2c160513d6393948b809049e6
Jeroen Haisma <J_Haisma@hotmail.com>
2019-07-01 10:13:53 -0300

    cleaning up auth

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-// const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+// Welcome Pager
+router.get('/', (req, res) => res.render('welcome'));
 
-// router.get('/', async (req, res) => {
-//   let books
-//   try {
-//     books = await Book.find().sort({ createdAt: 'desc' }).limit(10).exec()
-//   } catch {
-//     books = []
-//   }
-//   res.render('index', { books: books })
-// })
-
-
-// Welcome Page
- router.get('/', (req, res) => res.render('welcome'));
-
-// Dashboard
-// router.get('/dashboard', ensureAuthenticated, (req, res) =>
-//   res.render('dashboard', {
-//     user: req.user
-//   })
-// );
 
 module.exports = router
